fe174f5345e4614942ea6faf64fd9d51c12f5a14
joxtraex <fcarretero87@gmail.com>
2019-06-25 20:47:42 -0700

    Adding processing of path based on patterns
    Adding processing based on paths to look for patterns using the front.mp4, and generate a generic map of possible entries to limit the search, the secondary processing is disabled currently.

--- TeslaCamAnalyzer.py
+++ TeslaCamAnalyzer.py
@@ -8,10 +8,16 @@ from PyQt5.QtCore import pyqtSlot
 from pyqt5vlc import Player
 from FootageArchiverList import FootageArchiverList
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5.QtCore import QModelIndex
 
 # E:\TeslaCam
 class App(QDialog):
     list = None
+    model = None
+    modelList = None
+    basePath = None
+    basePathMap = None
+
     def __init__(self):
         super().__init__()
         self.title = 'PyQt5 layout - pythonspot.com'
@@ -53,16 +59,19 @@ class App(QDialog):
 
         button = QPushButton("Begin")
         button.clicked.connect(self.onClick)
+        button2 = QPushButton("Back")
+        button.clicked.connect(self.onClick2)
         layout.addWidget(button, 0, 3)
-        layout.addWidget(player1, 1, 0)
-        layout.addWidget(player2, 1, 1)
-        layout.addWidget(player3, 1, 2)
+        layout.addWidget(button2, 1, 3)
+        layout.addWidget(player1, 2, 0)
+        layout.addWidget(player2, 2, 1)
+        layout.addWidget(player3, 2, 2)
 
         self.list = QListView()
         self.list.setWindowTitle('Example List')
         self.list.setMinimumSize(600, 400)
         self.list.setGeometry(self.left, self.top+50, self.width, self.height)
-        layout.addWidget(self.list, 2, 0)
+        layout.addWidget(self.list, 3, 0)
 
         self.horizontalGroupBox.setLayout(layout)
 
@@ -70,21 +79,61 @@ class App(QDialog):
         print("processing")
         dialog_txt = "Choose Media File"
         filename = QFileDialog.getExistingDirectory(self, dialog_txt, os.path.expanduser('~'))
+        self.modelList = []
+        self.createListForDirectory(filename, None)
 
-        self.createListForDirectory(filename)
-
-    def createListForDirectory(self, file):
-        model = QStandardItemModel(self.list)
-        for f in os.listdir(file):
-            print("file: "+str(f))
-            item = QStandardItem(f)
-            model.appendRow(item)
-
-        self.list.setModel(model)
+    def onClick2(self):
+        lastModel = self.modelList.pop()
+        self.list.setModel(lastModel)
         self.list.show()
+        self.list.clicked.connect(self.processDirectory)
 
-    def processDirectory(self, fileList):
-        print("not implemented")
+
+    def createListForDirectory(self, baseDirectory, map):
+        self.model = QStandardItemModel(self.list)
+        self.modelList.append(self.model)
+        self.basePath = baseDirectory
+        for f in os.listdir(baseDirectory):
+            print("file: "+str(f))
+            if (os.path.isdir(os.path.join(baseDirectory, f))):
+                print("adding directory")
+                item = QStandardItem(f)
+                self.model.appendRow(item)
+
+        self.list.setModel(self.model)
+        self.list.show()
+        self.list.clicked.connect(self.processDirectory)
+
+    def processDirectory(self, modelIndex):
+        targetFile = self.model.itemFromIndex(modelIndex).text()
+        print("processing directory for: "+targetFile)
+
+        self.basePathMap = []
+        path = os.path.join(self.basePath, targetFile)
+        print( "processing directory in path: "+path)
+        for f in os.listdir(path):
+            targetFilePath = os.path.join(self.basePath, f)
+            #isFile = os.path.isfile(targetFilePath)
+            print("files in clicked directory: "+targetFilePath)
+            if "front.mp4" in f:
+                start = f.find("front.mp4")
+                baseName = f[0:start]
+                print("base path set as: "+baseName)
+                self.basePathMap.append(baseName)
+
+        #for path in basePaths:
+        for item in self.basePathMap:
+            print("path XXX: "+item)
+
+        #self.createListForDirectory(None, self.basePathMap)
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
